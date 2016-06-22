18540a8d9776098d4e3be9738341bc456e922510
Henry Fan <henry.fan@centraltrillion.com>
2016-06-22 22:55:17 +0800

    feat: add main tableView and tableViewCell

---
+++ Keyboard/MainTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MainTableViewCell.swift
+//  Keyboard
+//
+//  Created by HenryFan on 22/6/2016.
+//  Copyright © 2016 HenryFanDi. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var textField: UITextField!
+  
+  // MARK: Initialize
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
+  
+  // MARK: Public
+  
+  class func nib() -> UINib {
+    return UINib(nibName: "MainTableViewCell", bundle: nil)
+  }
+  
+  // MARK: Private
+  
+  private func setupMainTableViewCell() {
+  }
+  
+}
--- Keyboard/MainViewController.swift
+++ Keyboard/MainViewController.swift
@@ -8,16 +8,52 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  private let MainCellReuseIdentifier = "MainCellReuseIdentifier"
+  
+  @IBOutlet weak var tableView: UITableView!
   
   // MARK: Lifecycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupMainViewController()
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
+  
+  // MARK: Private
+  
+  private func setupMainViewController() {
+    setupTableView()
+  }
+  
+  private func setupTableView() {
+    tableView.registerNib(MainTableViewCell.nib(), forCellReuseIdentifier: MainCellReuseIdentifier)
+    
+    tableView.estimatedRowHeight = 50.0
+    tableView.rowHeight = UITableViewAutomaticDimension
+  }
+  
+  // MARK: UITableViewDataSource
+  
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let tableViewCell = tableView.dequeueReusableCellWithIdentifier(MainCellReuseIdentifier) as! MainTableViewCell
+    tableViewCell.backgroundColor = UIColor.redColor()
+    return tableViewCell
+  }
+  
+  // MARK: UITableViewDelegate
   
 }
